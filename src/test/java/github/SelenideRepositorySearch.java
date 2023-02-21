4706826c7d396dcdff591a019ca957275c06adbb
Alexei Vinogradov <6568032+vinogradoff@users.noreply.github.com>
2023-02-21 17:58:48 +0100

    initial

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -3,21 +3,12 @@ package github;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideRepositorySearch {
 
   @Test
   void shouldFindSelenideRepositoryAtTheTop() {
-    // открыть главную страницу
-    open("https://github.com/");
-    // ввести в поле поиска selenide и нажать enter
-    $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-    // кликнуть на первый репозиторий из списка найденых
-    $$("ul.repo-list li").first().$("a").click();
-    // проверка: заголовок selenide/selenide
-    $("#repository-container-header").shouldHave(text("selenide / selenide"));
+
   }
 }
